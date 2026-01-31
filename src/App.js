88d69c6bca5e2a64d68f661598918891c9a62d69
rajesh93986 <rajesh93986@gmail.com>
2026-01-31 12:33:07 +0530

    code changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      This is first deployment in vercel
+      git add .
+      git commit -m  "code changes"
+      git pull origin main
+      git push origin main
+    </>
+  );
+}
+
+export default App;
